285cd93c0e9879d4aff6e8919fbbb856230b2213
Grek Maria <99902188+hellene-mary@users.noreply.github.com>
2022-11-19 11:24:23 +0200

    remove excess

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -8,13 +8,10 @@ import css from './App.module.css';
 
 document.title = 'PhonebookBox_redux';
 
-// * +LocalStorage
-
 const App = () => {
   return (
     <div className={css.phonebookBox}>
       <h1>Phonebook</h1>
-
       <ContactForm />
       <h2>Contacts</h2>
       <Filter />
